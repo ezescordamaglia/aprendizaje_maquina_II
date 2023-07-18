331f7a5a899dc708b2c78a25796f1ebff292f29d
SantiagoGonzalezAchaval <santigonzalezachaval@gmail.com>
2023-07-17 22:35:06 -0300

    train_actualizado. Error en paths.

--- TP_Integrador/src/train.py
+++ TP_Integrador/src/train.py
@@ -3,13 +3,18 @@ train.py
 
 COMPLETAR DOCSTRING
 
-DESCRIPCIÓN:
-AUTOR:
-FECHA:
+DESCRIPCIÓN: Entrenamiento de modelo con dataframe final. Se exporta modelo entrenado .pkl.
+AUTOR: Ezquiel Scordamaglia - Santiago Gonzalez Achaval
+FECHA: 17/7/2023
 """
 
 # Imports
-
+import pandas as pd
+from sklearn.model_selection import train_test_split, cross_validate, cross_val_score
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+import pickle as pkl
+import os
 class ModelTrainingPipeline(object):
 
     def __init__(self, input_path, model_path):
@@ -20,32 +25,52 @@ class ModelTrainingPipeline(object):
         """
         COMPLETAR DOCSTRING 
         
-        :return pandas_df: The desired DataLake table as a DataFrame
+        Leer los datos del DataLake y retornarlos como un DataFrame.
+
+        :return: El DataFrame con los datos del DataLake.
         :rtype: pd.DataFrame
         """
             
         # COMPLETAR CON CÓDIGO
-        
+        pandas_df = pd.read_csv(f"{self.input_path}/train_final.csv")
+
         return pandas_df
 
     
     def model_training(self, df: pd.DataFrame) -> pd.DataFrame:
+        
         """
         COMPLETAR DOCSTRING
-        
+        Entrenar el modelo usando linear regression.
+        Usamos el dataset entero al no precisar de validacion en este punto.
         """
         
         # COMPLETAR CON CÓDIGO
-        
+        seed = 28
+        model = LinearRegression()
+
+        # División de dataset de entrenaimento y validación
+        X = df.drop(columns='Item_Outlet_Sales') #[['Item_Weight', 'Item_MRP', 'Outlet_Establishment_Year', 'Outlet_Size', 'Outlet_Location_Type']] # .drop(columns='Item_Outlet_Sales')
+        x_train, x_val, y_train, y_val = train_test_split(X, df['Item_Outlet_Sales'], test_size = 0, random_state=seed)
+
+        # Entrenamiento del modelo
+        model_trained = model.fit(x_train,y_train)
+
         return model_trained
 
     def model_dump(self, model_trained) -> None:
-        """
-        COMPLETAR DOCSTRING
         
         """
-        
-        # COMPLETAR CON CÓDIGO
+        Guarda el modelo entrenado en un archivo o ubicación específica.
+
+        :param model_trained: El modelo entrenado que se desea guardar.
+        :type model_trained: Any
+        """
+            
+        # COMPLETAR CON CÓDIGO PARA GUARDAR EL MODELO
+        file_path = f"{self.model_path}/trained_model.pkl"
+        with open(file_path, 'wb') as f:
+            pkl.dump(model_trained, f)
         
         return None
 
@@ -57,5 +82,5 @@ class ModelTrainingPipeline(object):
 
 if __name__ == "__main__":
 
-    ModelTrainingPipeline(input_path = 'Ruta/De/Donde/Voy/A/Leer/Mis/Datos',
-                          model_path = 'Ruta/Donde/Voy/A/Escribir/Mi/Modelo').run()
+    ModelTrainingPipeline(input_path = '../final_data',
+                          model_path = '../model').run()
